cf16371884351d9aaf98ce9c371375f83277fd83
Yazdan <128619328+Yazdish@users.noreply.github.com>
2023-06-30 16:43:50 +0330

    Update ReversedList.py

--- ReversedList.py
+++ ReversedList.py
@@ -1,7 +1,12 @@
-Array_1 = []
-x = input("enter sequence: ")
-y = x.split(",")
-for i in range(len(y)):
-    y[i] = int(y[i])
-y = y.reverse()
-print(y)
+userList = []
+Reversed = []
+
+while True:
+    user_input = input("enter number/word or E to Exit: ")
+    if user_input == "E":
+        break
+    else:
+        userList.append(user_input)
+for i in range(len(userList),0, -1):
+    Reversed.append(userList[i-1])
+print(Reversed)
