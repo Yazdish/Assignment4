baca6ffd34970529f561232c84cb0c39b0831d3c
Yazdan <128619328+Yazdish@users.noreply.github.com>
2023-06-30 16:25:22 +0330

    Update DelIdenticals.py

--- DelIdenticals.py
+++ DelIdenticals.py
@@ -1,11 +1,10 @@
-Array_1 = []
 x = input("enter sequence: ")
 y = x.split(",")
 for i in range(len(y)):
     y[i] = int(y[i])
 
 for i in range(len(y)):
-    for j in range(i+1, len(y)-1):
+    for j in range(i+1, len(y)):
         if y[i] == y[j]:
             y.pop(j)
         else:
